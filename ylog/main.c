ee3839461117ba0cafe3e0e76edde698c9c64574
pengyunchou <5740826@qq.com>
2015-01-06 08:38:52 +0800

    Initial Commit

---
+++ ylog/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ylog
+//
+//  Created by pengyunchou on 15-1-6.
+//  Copyright (c) 2015年 pengyunchou. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
